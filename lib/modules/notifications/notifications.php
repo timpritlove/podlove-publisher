6d9199889c26910405e6d6a07b00f2cfac4d3789
Alex <alexlueken@gmail.com>
2014-04-13 15:39:00 +0200

    register_module

---
+++ lib/modules/notifications/notifications.php
@@ -0,0 +1,18 @@
+<?php 
+namespace Podlove\Modules\Notifications;
+
+use Podlove\Model;
+
+class Notifications extends \Podlove\Modules\Base {
+
+	protected $module_name = 'Notifications';
+	protected $module_description = 'Handles Notifications on Podlove events.';
+	protected $module_group = 'system';
+
+	public function load() {
+		
+	}
+
+	
+
+}
